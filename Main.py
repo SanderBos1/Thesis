75ebcf2d601c09391b3716da325ab60afa89ef45
SanderBos1 <sanderb0ss@live.nl>
2022-10-18 09:02:58 +0200

    Finding topk 3 variables

--- Main.py
+++ Main.py
@@ -14,7 +14,7 @@ lag = int(windowlength/10)
 # conversion of csv file to desired data
 
 df = pd.read_pickle("Data/stocks_prepared.pkl")
-features_size = 5
+features_size = 30
 features = df.columns.tolist()
 
 features = features[0:features_size]
@@ -46,25 +46,35 @@ def Granger_causality(dep_var, features, df):
             scores.append([current, score])
     return scores
 
-def topk(x):
-    for i in range(0, len(x)-1):
-        if x[i][1] < x[i+1][1]:
-            return x
-    return [-1]
+def topk(x, topk):
+    difference = x[1][1] - x[0][1]
+    if len(topk) < 10 and x[1][1] > 0:
+        topk.append([x, difference])
+    elif len(topk) >= 10:
+        for i in topk:
+            if difference > i[1] and x[1][1] > 0:
+                topk = sorted(topk, key=lambda x: x[1])
+                topk.append([x, difference])
+                topk.remove(i)
+                break
+    topk = sorted(topk, key=lambda x: x[1])
+    return topk
 
-dep_var = []
-for i in range(len(features)-2):
+def finding_topk_granger():
+    dep_var = []
+    for i in range(len(features)-2):
         dep_var.append([features[i], features[i + 1],features[i+2]])
         dep_var.append([features[i+1], features[i], features[i + 2]])
 
-answers_spark = []
-spark = SparkSession.builder.master("local[1]") \
-    .appName("SparkByExamples.com").getOrCreate()
-rdd=spark.sparkContext.parallelize(dep_var)
-rdd2 = rdd.map(lambda x: Granger_causality(x, features, df))
-for element in rdd2.collect():
-    answers_spark.append(element)
-print(answers_spark)
-rdd3 = rdd2.map(lambda x: topk(x))
-for element in rdd3.collect():
-    print(element)
+    answers_spark = []
+    difference_topk = []
+    spark = SparkSession.builder.master("local[1]") \
+        .appName("SparkByExamples.com").getOrCreate()
+    rdd=spark.sparkContext.parallelize(dep_var)
+    rdd2 = rdd.map(lambda x: Granger_causality(x, features, df))
+    for element in rdd2.collect():
+        answer = topk(element, difference_topk)
+    return answer
+
+granger = finding_topk_granger()
+print(granger)
