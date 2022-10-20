207053eb0b109a5d70957a7d40fd0a64eb4218be
SanderBos1 <sanderb0ss@live.nl>
2022-10-20 21:58:15 +0200

    Cleanup and efficiency improvement. Still need to look at var of granger

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
-
-import pyspark.conf
+from itertools import combinations
 from VAR import Var
 from pyspark.sql import SparkSession
 
@@ -14,69 +13,54 @@ lag = int(windowlength/10)
 # conversion of csv file to desired data
 
 df = pd.read_pickle("Data/stocks_prepared.pkl")
-features_size = 40
+features_size = 5
 features = df.columns.tolist()
 
-features = features[0:features_size]
-
-for j in features:
-    # loop through each of the features
-    for i in range(1, lag + 1):
-        # add lag i of feature j to the dataframe
-            df[f"{j}_Lag_{i}"] = df[j].shift(i)
-df = df.dropna()
-
+features = list(features[0:features_size])
+df = df[features]
 
 # function that calculates if there is Granger causality
-def Granger_causality(dep_var, features, df):
-    notneeded = []
+def Granger_causality(dep_var):
     variances = []
     scores = []
 
-    for feature in features:
-            notneeded.append(feature)
     for i in range(len(dep_var)):
-        current = dep_var[:i+1]
-        VAR = Var(df, lag)
-        notneeded.remove(dep_var[i])
-        variance = VAR.varCalculation(current, notneeded)
+        current = list(dep_var[:i+1])
+        data = df[current].copy(deep=True)
+        VAR = Var(data, lag)
+        variance = VAR.varCalculation(current)
         variances.append(variance)
         if len(variances) > 1:
             score = np.log(variances[0]/variances[i])
             scores.append([current, score])
     return scores
 
-def topk(x, topk):
+def difference_calc_3(x):
     difference = x[1][1] - x[0][1]
-    if len(topk) < 10 and x[1][1] > 0:
-        topk.append([x, difference])
-    elif len(topk) >= 10:
-        topk = sorted(topk, key=lambda x: x[1])
-        for i in topk:
-            if difference > i[1] and x[1][1] > 0:
-                topk.append([x, difference])
-                topk.remove(i)
-                return topk
-    topk = sorted(topk, key=lambda x: x[1])
-    return topk
+    y = [x, difference]
+    return y
 
-def finding_topk_granger():
-    dep_var = []
-    for i in range(len(features)-2):
-        dep_var.append([features[i], features[i + 1],features[i+2]])
-        dep_var.append([features[i+1], features[i], features[i + 2]])
-        dep_var.append([features[i+2], features[i+1], features[i]])
-
-
-    difference_topk = []
+def finding_topk_granger_3():
+    # Makes a list of all combinations of stocks and creates a list
+    granger_variables = list(combinations(features, 3))
+    for i in granger_variables:
+        print(i)
+    # creates a sparksession to be used
     spark = SparkSession.builder.master("local[1]") \
         .appName("SparkByExamples.com").getOrCreate()
-    rdd=spark.sparkContext.parallelize(dep_var)
-    rdd2 = rdd.map(lambda x: Granger_causality(x, features, df))
-    for element in rdd2.collect():
-        difference_topk = topk(element, difference_topk)
-    return difference_topk
+    rdd = spark.sparkContext.parallelize(granger_variables)
 
-granger = finding_topk_granger()
+    # calculates the GC of bivariate and Multivariate combinations
+    rdd2 = rdd.map(lambda x: Granger_causality(x))
+
+    # calculates and returns an array of the difference between the combinations
+    rdd3 = rdd2.map(lambda x: difference_calc_3(x))
+
+    #takes the top 10 with the largest difference
+    rdd4 = rdd3.top(10, key=lambda x: x[1])
+    return rdd4
+
+
+granger = finding_topk_granger_3()
 for i in granger:
     print(i)
--- VAR.py
+++ VAR.py
@@ -1,6 +1,5 @@
-from Performance import Diagnostics
-from formulas import normalEquations, dotProduct
-
+import statistics
+import statsmodels.api as sm
 
 class Var:
 
@@ -17,45 +16,26 @@ class Var:
     e: the error term
     """
 
-    def varCalculation(self, variables, notwanted):
-        if isinstance(variables, str):
-            variables = variables
-        else:
-            variables = variables[0]
-        # drop columns that are not needed
-        features = self.data.columns
-        if len(variables) == 1:
-            self.data = self.data.filter(like=variables[0])
-        else:
-            for i in notwanted:
-                self.data = self.data.drop([i], axis=1)
-            for j in notwanted:
-                for i in range(1, self.lag + 1):
-                    self.data = self.data.drop([f"{j}_Lag_{i}"], axis=1)
+    def varCalculation(self, variables):
+
+        for j in variables:
+            # loop through each of the features
+            for i in range(1, self.lag + 1):
+                # add lag i of feature j to the dataframe
+                self.data[f"{j}_Lag_{i}"] = self.data[j].shift(i)
+        self.data = self.data.dropna()
+
         # extract the first variables.
-        y_True = self.data[variables]
+        y_True = self.data[variables[0]]
+
         self.data = self.data.drop(variables, axis=1)
 
-
-        # insert intercept column with all value of 1
-        self.data.insert(0, "Intercept", 1)
-
-        # transform to numpy for usage
-        y_True = y_True.to_numpy()
-        self.data = self.data.to_numpy()
-
-        # parameter estimate
-        b = normalEquations(self.data, y_True)
-
-        # Calculated the y values with the help of the predicted parameters b
-        y_values = []
-        for i in self.data:
-            y = dotProduct(i, b)
-            y_values.append(y)
-
-        diagntd = Diagnostics(y_values, y_True)
-        variance = diagntd.results()
-        return variance
+        model = sm.OLS(y_True, self.data)
+        results = model.fit()
+        resid = results.resid
+        var = statistics.variance(resid)
+        print("these are the resid", var)
+        return var
 
 
 
